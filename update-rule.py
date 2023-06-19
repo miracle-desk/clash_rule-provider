f042bd6cbd36411e81a0c672af567c2ece9d070a
An** <135781170+miracle-desk@users.noreply.github.com>
2023-06-19 07:02:02 +0700

    Update update-rule.py

--- update-rule.py
+++ update-rule.py
@@ -306,36 +306,90 @@ def get_rule_Phishing_URL(url):
     except Exception as e:
         print(e)
         return None
+def get_rule_StevenBlackList(url):
+    try:
+        r = requests.get(url)
+        update_rule_StevenBlackList = r.text.split("\n")
+        update_rule_StevenBlackList = [line.replace("||", "").replace("|", "").replace("://", "").replace("127.0.0.1", "").replace("0.0.0.0", "").replace("^", "") for line in update_rule_StevenBlackList if not line.startswith(('#', '!', '/', '@', '-', '&'))]
+        domains = []
+        ips = []
+        for line in update_rule_StevenBlackList:
+            if line:
+                if line[0].isdigit():
+                    # Jika baris dimulai dengan angka, itu kemungkinan adalah alamat IP
+                    try:
+                        # Coba parsing IP dengan modul ipaddress
+                        ip = ipaddress.ip_network(line.strip().split('$')[0])
+                        ips.append(ip.with_prefixlen)
+                    except ValueError:
+                        # Jika parsing gagal, abaikan baris ini
+                        pass
+                else:
+                    # Jika bukan alamat IP, itu kemungkinan adalah domain
+                    domain = line.split("$")[0].strip()
+                    if domain.startswith("*."):
+                        domain_suffix = domain + ""
+                        domains.append("  - DOMAIN-SUFFIX," + domain_suffix)                       
+                    elif domain.startswith("."):
+                        domain_suffix = domain + ""
+                        domains.append("  - DOMAIN-SUFFIX,*" + domain_suffix)
+                    elif domain.startswith("://"):
+                        domain_suffix = domain + ""
+                        domains.append("  - DOMAIN-SUFFIX,*." + domain_suffix)
+                    #Jika domain terdapat karakter tiktok, pinterest, twitter, linkedin, facebook, instagram, whatsapp maka dikategorikan sebagai DOMAIN-SUFFIX
+                    elif any(prefix in domain for prefix in ("tiktok", "pinterest", "twitter", "linkedin", "facebook","instagram", "whatsapp")):
+                        domain_suffix = domain + ""
+                        domains.append("  - DOMAIN-SUFFIX," + domain_suffix)
+                    else:
+                        domains.append("  - DOMAIN," + domain)
+        rules = domains + ["  - IP-CIDR," + ip for ip in ips]
+        rules.insert(0, "payload:")
+        return rules
+    except Exception as e:
+        print(e)
+        return None
 
 update_rule_AdGuardDNS_filter = get_AdGuardDNS_filter("https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt")
 if update_rule_AdGuardDNS_filter:
     with open("rule_AdGuardDNS-filter.yaml", "w", encoding='utf-8') as f:
-        f.write("\n".join(update_rule_AdGuardDNS_filter))    
+        f.write("\n".join(update_rule_AdGuardDNS_filter))  
+
 update_rule_oisd_full = get_rule_oisd_full("https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt")
 if update_rule_oisd_full:
     with open("rule_oisd-full.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_oisd_full))
+
 update_rule_oisd_small = get_rule_oisd_small("https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt")
 if update_rule_oisd_small:
     with open("rule_oisd-small.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_oisd_small))
+
 update_rule_AdAway = get_rule_AdAway("https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt")
 if update_rule_AdAway:
     with open("rule_AdAway.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_AdAway))
+
 update_rule_antiAD = get_rule_antiAD("https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt")
 if update_rule_antiAD:
     with open("rule_CHN-antiAD.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_antiAD)) 
+
 update_Dandelion_AntiMalware = get_rule_Dandelion_AntiMalware("https://adguardteam.github.io/HostlistsRegistry/assets/filter_12.txt")
 if update_Dandelion_AntiMalware:
     with open("rule_Dandelion-AntiMalware.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_Dandelion_AntiMalware))
+
 update_rule_Malicious_URLhaus = get_rule_Malicious_URLhaus("https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt")
 if update_rule_Malicious_URLhaus:
     with open("rule_Malicious-URLhaus.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_Malicious_URLhaus))
+
 update_rule_Phishing_URL = get_rule_Phishing_URL("https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt")
 if update_rule_Phishing_URL:
     with open("rule_Phishing-URL.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(update_rule_Phishing_URL))
+
+update_rule_StevenBlackList = get_rule_StevenBlackList("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-social-only/hosts")
+if update_rule_StevenBlackList:
+    with open("rule_StevenBlackList.yaml", "w", encoding='utf-8') as f:
+        f.write("\n".join(update_rule_StevenBlackList))
